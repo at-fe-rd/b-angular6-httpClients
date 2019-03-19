f78697ed64692af95719297a1b7f988d4d243ba5
quan do <quan.do@asiantech.vn>
2019-03-19 08:54:59 +0700

    first commit

---
+++ src/app/core/service/api/error-handler.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { DialogComponent } from '../../../shared/partial/dialog/dialog.component';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ErrorHandler {
+
+  constructor(
+    public dialog: MatDialog,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  public handleError(err: any) {
+    if (err.status === 0) {
+      let dialogRef = this.dialog.open(DialogComponent, {});
+      dialogRef.componentInstance.type = 'msg_refresh_dlg';
+    }
+    else if (err.status === 401) {
+      if (!this.dialog.openDialogs.length) {
+        this.auth.logout();
+        let dialogRef = this.dialog.open(DialogComponent, {});
+        dialogRef.componentInstance.type = 'msg_auth_dlg';
+      }
+    }
+    else if (err.status === 404) {
+      if (!this.dialog.openDialogs.length) {
+        let dialogRef = this.dialog.open(DialogComponent, {});
+        dialogRef.componentInstance.type = 'msg_404_dlg';
+      }
+    } 
+    else if (err.status === 403) {
+      if (!this.dialog.openDialogs.length) {
+        let dialogRef = this.dialog.open(DialogComponent, {
+          data: err.error.errors
+        });
+        dialogRef.componentInstance.type = 'custom_msg_dlg';
+      }
+    }
+    else if (err.status === 500) {
+      let dialogRef = this.dialog.open(DialogComponent, {});
+      dialogRef.componentInstance.type = 'msg_refresh_dlg';
+    }
+    else if (err.status === 422 && this.router.url !== '/auth/login') {
+      let dialogRef = this.dialog.open(DialogComponent, {});
+      dialogRef.componentInstance.type = 'msg_sys_dlg';
+    }
+  }
+}
